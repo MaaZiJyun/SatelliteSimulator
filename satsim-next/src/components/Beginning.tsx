55a6e99aded9420bc9d1eb3a12547ba14e874bb8
MaaZiJyun <97087257+MaaZiJyun@users.noreply.github.com>
2025-04-23 19:58:39 +0800

    refactor: update Beginning component to change title from "O.R.B.I.T" to "SatSim" and improve layout styling

--- satsim-next/src/components/Beginning.tsx
+++ satsim-next/src/components/Beginning.tsx
@@ -39,39 +39,40 @@ export default function Beginning() {
   };
 
   return (
-    <div className="h-full w-full flex flex-col items-center justify-center">
-      {/* Big Title */}
-      <h1 className="text-5xl font-bold mb-8">Welcome to O.R.B.I.T</h1>
+    <div className="h-full w-full flex flex-col items-center justify-center bg-black/50">
+      <div className="w-1/2 flex flex-col items-center justify-center">
+        {/* Big Title */}
+        <h1 className="text-5xl font-bold mb-8">{("Welcome to SatSim").toUpperCase()}</h1>
 
-      {/* Drag-and-Drop Container */}
-      <div
-        onDrop={handleDrop}
-        onDragOver={(e) => e.preventDefault()}
-        className="w-96 h-48 border-4 border-dashed border-[#00ffff] rounded-lg flex items-center justify-center text-center p-4 hover:bg-[#00ffff] transition"
-      >
-        {error && <div className="text-red-500">{error}</div>}
-        {!error && (
-          <div className="text-gray-500">
-            Drag and drop a JSON file here to get started.
-          </div>
-        )}
-      </div>
-
-      {/* File Input */}
-      <div className="mt-4">
-        <label
-          htmlFor="file-upload"
-          className="cursor-pointer px-4 py-2 bg-[#00ffff] text-black rounded-lg hover:bg-[#00cccc] transition"
+        {/* Drag-and-Drop Container */}
+        <div
+          onDrop={handleDrop}
+          onDragOver={(e) => e.preventDefault()}
+          className="w-full h-45 border-4 border-dashed border-[#00ffff] rounded-lg flex items-center justify-center text-center p-4 hover:bg-[#00ffff]/10 transition"
         >
-          Select a File
-        </label>
-        <input
-          id="file-upload"
-          type="file"
-          accept=".json"
-          className="hidden"
-          onChange={handleFileInputChange}
-        />
+          {error && <div className="text-red-500">{error}</div>}
+          {!error && (
+            <div className="text-gray-400">
+              Drag and drop a JSON file here to get started.
+            </div>
+          )}
+        </div>
+        {/* File Input */}
+        <div className="w-full mt-4">
+          <label
+            htmlFor="file-upload"
+            className="flex w-full items-center justify-center cursor-pointer px-4 py-2 bg-[#00ffff] text-black rounded-lg hover:bg-[#00cccc] transition"
+          >
+            Select a File
+          </label>
+          <input
+            id="file-upload"
+            type="file"
+            accept=".json"
+            className="flex w-full hidden"
+            onChange={handleFileInputChange}
+          />
+        </div>
       </div>
     </div>
   );
